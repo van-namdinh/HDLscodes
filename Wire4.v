4adc537fda56fa529bbfc7a897f65e8d8b447ca4
van-namdinh <vndprogramminges@gmail.com>
2018-08-17 16:08:09 +0700

    August182018

---
+++ Wire4.v
@@ -0,0 +1,18 @@
+/*
+ * Project name   :
+ * File name      : Wire4.v
+ * Created date   : Th08 17 2018
+ * Author         : Van-Nam DINH 
+ * Last modified  : Th08 17 2018 14:23
+ * Desc           :
+ */
+
+module top_module(
+    input a,b,c,
+    output  w,x,y,z);
+assign w = a;
+assign x = b;
+assign y = b;
+assign z = c;
+endmodule
+
